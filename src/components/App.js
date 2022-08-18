432946a53cb1a27bea26c3c9ada4a8813d9cfddf
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-08-18 20:57:57 +0200

    Add the function handleDone() so when you click on one li you can put the propierty of complete or invert it

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import '../styles/App.scss';
 
-// import { useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const tasks = [
-    {task: 'Comprar harina, jamón y pan rallado', completed: true},
-    {task: 'Hacer croquetas ricas', completed: true},
-    {task: 'Ir a la puerta de un gimnasio', completed: false},
+  const [tasks, setTasks] = useState([
+    {id: '1', task: 'Comprar harina, jamón y pan rallado', completed: true},
+    {id: '2', task: 'Hacer croquetas ricas', completed: true},
+    {id: '3', task: 'Ir a la puerta de un gimnasio', completed: false},
     {
-      task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio',
-      completed: false,
-    },
-  ];
+      id: '4', task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+      completed: false
+    }
+  ]);
+
+  const handleDone = (event) =>{
+    const clickedDoneId = event.currentTarget.id;
+    const foundTaskDone = tasks.find((tasks) => tasks.id === clickedDoneId);
+    foundTaskDone.completed =!foundTaskDone.completed;
+    setTasks([...tasks]);
+  }
 
   const renderTasks = () => {
-    return tasks.map((tasks, index) => {
+    return tasks.map((tasks) => {
       let highLight = null;
       if (tasks.completed === true){
         highLight = 'coloredHighLight';
@@ -23,7 +30,7 @@ function App() {
         highLight = '';
       }
       return (
-      <li key={index} className={highLight}>
+      <li key={tasks.id} id={tasks.id} className={highLight} onClick={handleDone}>
         {tasks.task}
       </li>)
     })
